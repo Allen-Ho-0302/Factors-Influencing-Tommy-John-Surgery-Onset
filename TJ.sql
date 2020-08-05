5651657493d2dd4082755536e7add79659408155
Allen-Ho-0302 <65414603+Allen-Ho-0302@users.noreply.github.com>
2020-08-04 22:04:31 -0400

    Delete TJ.sql

--- TJ.sql
+++
@@ -1,6 +0,0 @@
-select distinct t.Player, t.Position, t.Throws, t.date_of_surgery, p.height, p.weight, datediff(day, p.debut, t.date_of_surgery) as daydiff
-from TJ$ t
-join People$ p
-on t.Player=concat(nameFirst,' ',nameLast)
-where p.height is not null and p.weight is not null and datediff(day, p.debut, t.date_of_surgery)>0 and datediff(day, p.debut, t.date_of_surgery)<7000
-order by t.Player;
